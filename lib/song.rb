7d4fb0af2a021a7d15e1188f6a7fd884d2f12653
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-05 17:34:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,11 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
+    if song = self.find_by_name(name)
+     song
     else 
       self.create_by_name(name)
     end
   end
+  
 end
